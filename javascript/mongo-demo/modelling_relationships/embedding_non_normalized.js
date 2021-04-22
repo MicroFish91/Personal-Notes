fc0df15003006bf195687080c02cbc1204a923c0
unknown <mwfisher91@gmail.com>
2021-04-22 12:56:36 -0700

    Added more CSS notes

---
+++ javascript/mongo-demo/modelling_relationships/embedding_non_normalized.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+const Course = mongoose.model('Course', new mongoose.Schema({
+  name: String,
+  author: {
+    type: authorSchema,
+    required: true
+  }
+}));
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name, 
+    author
+  }); 
+  
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() { 
+  const courses = await Course.find();
+  console.log(courses);
+}
+
+async function updateAuthor(courseId){
+  // const course = await Course.update({ _id: courseId },
+  //   $set: {
+  //     'author.name': 'John Smith'
+  // });
+
+  // To remove author schema object (sub document)
+  const course = await Course.update({ _id: courseId },
+    $unset: {
+      'author': ''
+  });
+}
+
+async function addAuthor(courseId, author){
+  const course = await Course.findById(courseId);
+  course.authors.push(author);
+  course.save();
+}
+
+async function removeAuthor(courseId, authorId){
+  const course = await Course.findById(courseId);
+  const author = course.authors.id(authorId);
+  author.remove();
+  course.save();
+}
+
+createCourse('Node Course', new Author({ name: 'Matt' }));
