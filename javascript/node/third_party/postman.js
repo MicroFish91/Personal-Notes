02406d83ff83ecbcb4529c15a885e8f1467804e8
unknown <mwfisher91@gmail.com>
2021-05-12 19:43:14 -0700

    051221

--- javascript/node/third_party/postman.js
+++ javascript/node/third_party/postman.js
@@ -1,5 +1,5 @@
 // Calling endpoints using Postman
-// Chrome browser extension
+// Chrome browser extension or dl desktop app
 
 // Postman is an API development tool which helps to build, test, and modify APIs.
 // It has the ability to make various types of HTTP Requests(GET POST PUT PATCH etc)
